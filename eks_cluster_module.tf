f59e647a46e712fcbc188a7af27f3bd11bb8106c
andre <andrinho@gmail.com>
2025-06-29 12:18:56 -0300

    add bunch of stuff to cluster module

--- eks_cluster_module.tf
+++ eks_cluster_module.tf
@@ -7,17 +7,27 @@ resource "aws_eks_cluster" "eks_cluster" {
   }
 
   role_arn = aws_iam_role.cluster_role.arn
-  version  = "1.31"
+  version  = var.cluster_version
 
   vpc_config {
     subnet_ids = module.vpc.public_subnets
+    endpoint_private_access = var.cluster_endpoint_private_access
+    endpoint_public_access  = var.cluster_endpoint_public_access
+    public_access_cidrs     = var.cluster_endpoint_public_access_cidrs    
   }
+
+  kubernetes_network_config {
+    service_ipv4_cidr = var.cluster_service_ipv4_cidr
+  }
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
   # Ensure that IAM Role permissions are created before and deleted
   # after EKS Cluster handling. Otherwise, EKS will not be able to
   # properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.cluster_attach_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.cluster_attach_AmazonEKSVPCResourceController,
   ]
 }
 
@@ -25,3 +35,4 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster_role.name
 }
+
