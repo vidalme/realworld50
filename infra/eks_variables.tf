7e0b5808ae281241365db9147545abdb7ac682a4
andre <andrinho@gmail.com>
2025-06-30 02:13:08 -0300

    fix outputs and variables files

--- infra/eks_variables.tf
+++ infra/eks_variables.tf
@@ -1,89 +1,34 @@
-output "cluster_id" {
-  description = "The name/id of the EKS cluster."
-  value       = aws_eks_cluster.eks_cluster.id
+# variable "cluster_name" {
+#     description = "The name of the EKS cluster"
+#     type = string
+# }
+
+variable "cluster_service_ipv4_cidr" {
+  description = "service ipv4 cidr for the kubernetes cluster"
+  type        = string
+  default     = null
 }
 
-output "cluster_arn" {
-  description = "The Amazon Resource Name (ARN) of the cluster."
-  value       = aws_eks_cluster.eks_cluster.arn
+variable "cluster_version" {
+  description = "Kubernetes minor version to use for the EKS cluster (for example 1.31)"
+  type        = string
+  default     = null
 }
 
-output "cluster_certificate_authority_data" {
-  description = "Nested attribute containing certificate-authority-data for your cluster. This is the base64 encoded certificate data required to communicate with your cluster."
-  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+variable "cluster_endpoint_private_access" {
+  description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled."
+  type        = bool
+  default     = false
 }
 
-output "cluster_endpoint" {
-  description = "The endpoint for your EKS Kubernetes API."
-  value       = aws_eks_cluster.eks_cluster.endpoint
+variable "cluster_endpoint_public_access" {
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled. When it's set to `false` ensure to have a proper private access with `cluster_endpoint_private_access = true`."
+  type        = bool
+  default     = true
 }
 
-output "cluster_version" {
-  description = "The Kubernetes server version for the EKS cluster."
-  value       = aws_eks_cluster.eks_cluster.version
+variable "cluster_endpoint_public_access_cidrs" {
+  description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint."
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
 }
-
-output "cluster_iam_role_name" {
-  description = "IAM role name of the EKS cluster."
-  value       = aws_iam_role.eks_master_role.name 
-}
-
-output "cluster_iam_role_arn" {
-  description = "IAM role ARN of the EKS cluster."
-  value       = aws_iam_role.eks_master_role.arn
-}
-
-output "cluster_oidc_issuer_url" {
-  description = "The URL on the EKS cluster OIDC Issuer"
-  value       = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
-}
-
-output "cluster_primary_security_group_id" {
-  description = "The cluster primary security group ID created by the EKS cluster on 1.14 or later. Referred to as 'Cluster security group' in the EKS console."
-  value       = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
-}
-
-# EKS Node Group Outputs - Public
-output "node_group_public_id" {
-  description = "Public Node Group ID"
-  value       = aws_eks_node_group.eks_ng_public.id
-}
-
-output "node_group_public_arn" {
-  description = "Public Node Group ARN"
-  value       = aws_eks_node_group.eks_ng_public.arn
-}
-
-output "node_group_public_status" {
-  description = "Public Node Group status"
-  value       = aws_eks_node_group.eks_ng_public.status 
-}
-
-output "node_group_public_version" {
-  description = "Public Node Group Kubernetes Version"
-  value       = aws_eks_node_group.eks_ng_public.version
-}
-
-# EKS Node Group Outputs - Private
-/*
-output "node_group_private_id" {
-  description = "Node Group 1 ID"
-  value       = aws_eks_node_group.eks_ng_private.id
-}
-
-output "node_group_private_arn" {
-  description = "Private Node Group ARN"
-  value       = aws_eks_node_group.eks_ng_private.arn
-}
-
-output "node_group_private_status" {
-  description = "Private Node Group status"
-  value       = aws_eks_node_group.eks_ng_private.status 
-}
-
-output "node_group_private_version" {
-  description = "Private Node Group Kubernetes Version"
-  value       = aws_eks_node_group.eks_ng_private.version
-}
-
-*/
