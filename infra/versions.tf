fb2c09d6b9f363ed87e2ed3c8463df2dbcbff26d
andre <andrinho@gmail.com>
2025-07-03 22:38:17 -0300

    add remote state s3 for infra

--- infra/versions.tf
+++ infra/versions.tf
@@ -6,4 +6,18 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "" {
+    
+  }
+
+  backend "s3" {
+    bucket = "eks-terraform-andre"
+    key    = "dev/eks-cluster/terraform.tfstate"
+    region = "us-west-2" 
+ 
+    # For State Locking
+    dynamodb_table = "dev-ekscluster"    
+  }  
 }
+
